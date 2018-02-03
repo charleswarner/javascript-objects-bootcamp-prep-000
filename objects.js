497fddc24f082e9ec5f67c6f16af841ade642fc7
Charlie Warner <charlie@omnium-events.com>
2018-02-03 02:01:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({ artistName: songTitle })
+var playlist = new Object ({ artistName: "songTitle" })
 
 function updatePLaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
