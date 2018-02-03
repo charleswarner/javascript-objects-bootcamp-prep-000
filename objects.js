b7a6bb580c41c4418f5431353f471dd91ff33571
Charlie Warner <charlie@omnium-events.com>
2018-02-03 02:06:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { artistName: "songTitle" }
 function updatePLaylist(playlist, artistName, songTitle) {
   return playlist.artistName = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  
+}
